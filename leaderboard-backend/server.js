af2b921e7e9f835c86d8cc3cec259acfacac84c5
apts18o9 <aptevarad90@gmail.com>
2025-07-18 14:17:29 +0530

    updated cors

--- leaderboard-backend/server.js
+++ leaderboard-backend/server.js
@@ -12,7 +12,23 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI
 
-app.use(cors())
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://leaderboard-frontend-3d2o.onrender.com/' 
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+    },
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
+
 app.use(express.json());
 
 mongoose.connect(MONGO_URI, {
